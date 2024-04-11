36c3f507b272130a15ba0176289c2274a06a6f13
hirofumikanda <nandakandahirofumi322@yahoo.co.jp>
2024-04-11 20:43:05 +0900

    中回復アイテムと大回復アイテムを追加

--- item.py
+++ item.py
@@ -1,24 +1,20 @@
 import pygame as pg
 import random
 
+# アイテムスーパークラス
 class Item():
     def __init__(self):
         x = random.randint(100, 500)
         y = -100
-        self._image = pg.image.load("images/item1.png")
+        self._image = pg.image.load("images/portion.png")
         self._rect = pg.Rect(x, y, 50, 50)
         self._vx = 0
         self._vy = random.uniform(1, 4)
         self._hpRecovery = 0
-        self._is_existed = True
-    
+
     @property
     def rect(self):
         return self._rect
-    
-    @property
-    def is_existed(self):
-        return self._is_existed
     
     @property
     def hpRecovery(self):
@@ -33,18 +29,39 @@ class Item():
     def draw(self, screen): # 描画処理
         screen.blit(self._image, self._rect)
 
-class RecoveryItem(Item):
+# 小回復アイテム
+class SmallRecoveryItem(Item):
     def __init__(self):
         super().__init__()
-        self._image = pg.image.load("images/item1.png")
+        self._image = pg.image.load("images/portion.png")
         self._hpRecovery = 50
+
+# 中回復アイテム
+class MediumRecoveryItem(Item):
+    def __init__(self):
+        super().__init__()
+        self._image = pg.image.load("images/onigiri.png")
+        self._hpRecovery = 100
+        self._vy = random.uniform(5, 7)
+
+# 大回復アイテム
+class LargeRecoveryItem(Item):
+    def __init__(self):
+        super().__init__()
+        self._image = pg.image.load("images/donuts.png")
+        self._hpRecovery = 150
+        self._vy = random.uniform(8, 10)
 
 class ItemFactory():
     def create(self, itype): # タイプ指定で作る
-        if itype == "recovery":
-            return RecoveryItem()
+        if itype == "smallrecovery":
+            return SmallRecoveryItem()
+        if itype == "mediumrecovery":
+            return MediumRecoveryItem()
+        if itype == "largerecovery":
+            return LargeRecoveryItem()
         return Item()
     
     def random_create(self): # ランダムに作る
-        itype = random.choice(["recovery"])
+        itype = random.choice(["smallrecovery", "mediumrecovery", "largerecovery"])
         return self.create(itype)

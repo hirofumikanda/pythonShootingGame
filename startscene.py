9e065e84efd86e379ebe603391e27db3ca864281
hirofumikanda <nandakandahirofumi322@yahoo.co.jp>
2024-04-13 21:45:06 +0900

    スタート画面に難易度選択ボタンを追加

--- startscene.py
+++ startscene.py
@@ -6,10 +6,13 @@ import const
 '''
 class StartScene():
     def __init__(self, game):
-        font = pg.font.Font(None, 30)
+        # font = pg.font.Font(None, 30)
         self._game = game
-        self._msg = font.render("Press E(Easy) or N(Normal) or H(Hard) to play.", True, pg.Color("WHITE"))
+        # self._msg = font.render("Press E(Easy) or N(Normal) or H(Hard) to play.", True, pg.Color("WHITE"))
         self._gamestart = pg.image.load("images/gamestart.png")
+        self._easyButton = pg.image.load("images/EasyButton.png")
+        self._normalButton = pg.image.load("images/NormalButton.png")
+        self._hardButton = pg.image.load("images/HardButton.png")
     
     '''
     更新処理
@@ -27,6 +30,9 @@ class StartScene():
     描画処理
     '''
     def draw(self, screen):
-        screen.blit(self._msg, (70, 380))
+        # screen.blit(self._msg, (70, 380))
         if self._game.is_started == False:
-            screen.blit(self._gamestart, (50, 200))
+            # screen.blit(self._gamestart, (50, 200))
+            screen.blit(self._easyButton, (125,130))
+            screen.blit(self._normalButton, (125, 270))
+            screen.blit(self._hardButton, (125, 410))

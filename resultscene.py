3a3118b1f7609ff6181759070c1f64238f5eb9ce
hirofumikanda <nandakandahirofumi322@yahoo.co.jp>
2024-04-14 15:06:30 +0900

    結果表示にイラスト追加

--- resultscene.py
+++ resultscene.py
@@ -7,25 +7,31 @@ class ResultScene():
     def __init__(self, game):
         font = pg.font.Font(None, 50)
         self._game = game
-        self._msg = font.render("Press BACKSPACE to end.", True, pg.Color("WHITE"))
+        self._msg = font.render("Press SPACE to end.", True, pg.Color("WHITE"))
         self._gameover = pg.image.load("images/gameover.png")
         self._gameclear = pg.image.load("images/gameclear.png")
+        self._great = pg.image.load("images/great.png")
+        self._pity = pg.image.load("images/pity.png")
     
     '''
     更新処理
     '''
     def update(self):
         key = pg.key.get_pressed()
-        if key[pg.K_BACKSPACE]:
+        if key[pg.K_SPACE]:
             self._game.reset()
     
     '''
     描画処理
     '''
     def draw(self, screen):
-        screen.blit(self._msg, (90, 380))
+        screen.blit(self._msg, (120, 100))
         if self._game.is_playing == False:
+            # ゲームクリア時
             if self._game.is_cleared == True:
                 screen.blit(self._gameclear, (50, 200))
+                screen.blit(self._great, (237, 350))
+            # ゲームオーバー時
             else:
                 screen.blit(self._gameover, (50, 200))
+                screen.blit(self._pity, (237, 350))
